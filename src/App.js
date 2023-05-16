c5984dd96bb1ed8e53b9adb44ca1a0a7206aa978
mmesander <mark_mesander@hotmail.com>
2023-05-16 13:49:59 +0200

    Bonusopdracht af en als extraatje de waarde van de leeftijd niet als string maar als getal meegegeven in de functie

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    const [nameValue, setNameValue] = React.useState('');
-    const [ageValue, setAgeValue] = React.useState(0);
-    const [messageValue, setMessageValue] = React.useState('');
-    const [newsLetterValue, setNewsLetterValue] = React.useState(false);
+    // const [nameValue, setNameValue] = React.useState('');
+    // const [ageValue, setAgeValue] = React.useState(0);
+    // const [messageValue, setMessageValue] = React.useState('');
+    // const [newsLetterValue, setNewsLetterValue] = React.useState(false);
+    const [formState, setFormState] = React.useState({
+        name: '',
+        age: 0,
+        message: '',
+        newsletter: false,
+        gender: 'neutral',
+    });
+
+    function handleFormChange(e) {
+        const changedFieldName = e.target.name
+        let newValue = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+        if (changedFieldName === "age") {
+            newValue = parseInt(newValue);
+        }
+
+        setFormState({
+            ...formState,
+            [changedFieldName]: newValue
+        });
+    }
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(nameValue, ageValue, messageValue, newsLetterValue);
+        console.log(formState);
     }
 
     return (
@@ -25,8 +46,8 @@ function App() {
                                 type="text"
                                 id="name-field"
                                 name="name"
-                                value={nameValue}
-                                onChange={(e) => setNameValue(e.target.value)}
+                                value={formState.name}
+                                onChange={handleFormChange}
                             />
                         </label>
 
@@ -36,10 +57,24 @@ function App() {
                                 type="number"
                                 id="age-field"
                                 name="age"
-                                value={ageValue}
-                                onChange={(e) => setAgeValue(parseInt(e.target.value))}
+                                value={formState.age}
+                                onChange={handleFormChange}
                             />
                         </label>
+
+                        <label htmlFor="gender">
+                            Gender:
+                            <select
+                                name="gender"
+                                value={formState.gender}
+                                onChange={handleFormChange}>
+                                <option value="neutral">Neutral</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                        </label>
+
+
                     </fieldset>
 
                     <fieldset>
@@ -53,9 +88,8 @@ function App() {
                                 placeholder="Wat vond je van het recept"
                                 cols="45"
                                 rows="4"
-                                value={messageValue}
-                                onChange={(e) => setMessageValue(e.target.value)}
-                            />
+                                value={formState.message}
+                                onChange={handleFormChange}                            />
                         </label>
 
                         <label htmlFor="newsletter">
@@ -63,9 +97,8 @@ function App() {
                                 type="checkbox"
                                 id="newsletter"
                                 name="newsletter"
-                                checked={newsLetterValue}
-                                onChange={() => setNewsLetterValue(!newsLetterValue)}
-                            />
+                                checked={formState.newsletter}
+                                onChange={handleFormChange}                            />
                             Ik schrijf me in voor de nieuwsbrief
                         </label>
                         <button type="submit">
