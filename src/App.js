7a6f1c2c70d975fc271dd02e988a2d1ea6e113e7
mmesander <mark_mesander@hotmail.com>
2023-05-16 12:02:42 +0200

    Eerste form gemaakt

--- src/App.js
+++ src/App.js
@@ -2,11 +2,39 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Maak hier jouw formulier!
-    </div>
-  );
+    const [nameValue, setNameValue] = React.useState('');
+    const [ageValue, setAgeValue] = React.useState(0)
+
+    return (
+        <>
+            <div>
+                <form>
+                    <fieldset>
+                        <legend>Gegevens</legend>
+                        <label htmlFor="name-field">Naam:
+                            <input
+                                type="text"
+                                id="name-field"
+                                name="name"
+                                value={nameValue}
+                                placeholder="Bert Bollemans"
+                                onChange={(e) => setNameValue(e.target.value)}
+                            />
+                        </label>
+                        <label htmlFor="age-field">Leeftijd:
+                            <input
+                                type="number"
+                                id="age-field"
+                                name="age"
+                                value={ageValue}
+                                onChange={(e) => setAgeValue(parseInt(e.target.value))}
+                            />
+                        </label>
+                    </fieldset>
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default App;
