b845b97b2b944c6098cf0cc1496f035f0f8fa741
mmesander <mark_mesander@hotmail.com>
2023-05-16 12:30:50 +0200

    Foutje uit de code gehaald

--- src/App.js
+++ src/App.js
@@ -14,70 +14,69 @@ function App() {
 
     return (
         <>
-        <div>
-            <form onSubmit={handleSubmit}>
-                <fieldset>
-                    <legend>Gegevens</legend>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <fieldset>
+                        <legend>Gegevens</legend>
 
-                    <label htmlFor="name-field">
-                        Naam:
-                        <input
-                            type="text"
-                            id="name-field"
-                            name="name"
-                            value={nameValue}
-                            onChange={(e) => setNameValue(e.target.value)}
-                        />
-                    </label>
+                        <label htmlFor="name-field">
+                            Naam:
+                            <input
+                                type="text"
+                                id="name-field"
+                                name="name"
+                                value={nameValue}
+                                onChange={(e) => setNameValue(e.target.value)}
+                            />
+                        </label>
 
-                    <label htmlFor="age-field">
-                        Leeftijd:
-                        <input
-                            type="number"
-                            id="age-field"
-                            name="age"
-                            value={ageValue}
-                            onChange={(e) => setAgeValue(parseInt(e.target.value))}
-                        />
-                    </label>
-                </fieldset>
+                        <label htmlFor="age-field">
+                            Leeftijd:
+                            <input
+                                type="number"
+                                id="age-field"
+                                name="age"
+                                value={ageValue}
+                                onChange={(e) => setAgeValue(parseInt(e.target.value))}
+                            />
+                        </label>
+                    </fieldset>
 
-                <fieldset>
-                    <legend>Jouw review</legend>
+                    <fieldset>
+                        <legend>Jouw review</legend>
 
-                    <label htmlFor="message-field">
-                        Opmerkingen:
-                        <textarea
-                            id="message-field"
-                            name="message"
-                            placeholder="Wat vond je van het recept"
-                            cols="45"
-                            rows="4"
-                            value={messageValue}
-                            onChange={(e) => setMessageValue(e.target.value)}
-                        />
-                    </textare>
-                </label>
+                        <label htmlFor="message-field">
+                            Opmerkingen:
+                            <textarea
+                                id="message-field"
+                                name="message"
+                                placeholder="Wat vond je van het recept"
+                                cols="45"
+                                rows="4"
+                                value={messageValue}
+                                onChange={(e) => setMessageValue(e.target.value)}
+                            />
+                        </label>
 
-                <label htmlFor="newsletter">
-                    <input
-                        type="checkbox"
-                        id="newsletter"
-                        name="newsletter"
-                        checked={newsLetterValue}
-                        onChange={() => setNewsLetterValue(!newsLetterValue)}
-                    />
-                    Ik schrijf me in voor de nieuwsbrief
-                </label>
-                <button type="submit">
-                    Versturen
-                </button>
-            </fieldset>
-        </form>
-        </div>
-</>
-)
-    ;
+                        <label htmlFor="newsletter">
+                            <input
+                                type="checkbox"
+                                id="newsletter"
+                                name="newsletter"
+                                checked={newsLetterValue}
+                                onChange={() => setNewsLetterValue(!newsLetterValue)}
+                            />
+                            Ik schrijf me in voor de nieuwsbrief
+                        </label>
+                        <button type="submit">
+                            Versturen
+                        </button>
+                    </fieldset>
+                </form>
+            </div>
+        </>
+    )
+        ;
 }
 
 export default App;
